08e98caf31ced3fae9fecd3651cef0beb3c4de68
임동영 <imdong207@gmail.com>
2026-03-06 10:23:14 +0900

    fix: (#53) 프론트와 서버 연동 시 CORS 문제 해결

--- app/app.js
+++ app/app.js
@@ -4,10 +4,17 @@ const express = require("express");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
